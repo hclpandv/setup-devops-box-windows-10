86a58404f0f7e73aaa05c4c7a3101f115d658b97
Vikas Pandey <vikaspa6@gmail.com>
2019-03-27 11:03:48 +0530

    Update InstallWsl.ps1

--- InstallWsl.ps1
+++ InstallWsl.ps1
@@ -1,7 +1,1 @@
-try {
-    Write-ChocolateySuccess 'Installing wsl'
-    choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
-} catch {
-  Write-ChocolateyFailure 'MyPackage' $($_.Exception.Message)
-  throw
-}
+choco install -y Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
